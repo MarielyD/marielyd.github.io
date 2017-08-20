936dfa7537a55ed76a1bd87563da5ed545be269a
Mariely Delacruz <mdlc0502@gmail.com>
2017-08-20 16:26:11 -0400

    js file

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -15,3 +15,6 @@ function Recursive()
   	document.getElementById("adj").innerHTML= adjTab[i];
   setTimeout ("Recursive()", 500);
 }
+
+var contactform =  document.getElementById('contactform');
+    contactform.setAttribute('action', '//formspree.io/' + 'mdlc0502' + '@' + 'gmail' + '.' + 'com');
