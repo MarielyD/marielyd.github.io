292d3a8ac03db0474fb011d79d3beae07d59c0cb
Mariely Delacruz <mdlc0502@gmail.com>
2017-08-20 14:46:38 -0400

    styling

---
+++ assets/portfolio.js
@@ -0,0 +1,17 @@
+var adjTab =["designer.","life long learner.", "creative.","problem solver.", "dreamer."];
+var i = 0;
+window.onload =function words()
+{
+
+	document.getElementById("adj").innerHTML= adjTab[i];
+	setTimeout ("Recursive()", 500);
+};
+
+function Recursive()
+{
+  i++
+	if( i == 5 )
+		i = 0;
+  	document.getElementById("adj").innerHTML= adjTab[i];
+  setTimeout ("Recursive()", 500);
+}
